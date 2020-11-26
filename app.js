68b29d0cbf83f5b9268bebf9828c7ff3a0cee3ff
Ellen Ho <ellen31725@gmail.com>
2020-11-27 07:20:17 +0800

    add basic structure

---
+++ app.js
@@ -0,0 +1,29 @@
+// app.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Required log format: 2019-5-17 18:51:12 | GET from /
+// 在路由系統中，對所有請求設定統一前置處理
+// 瞭解時間戳記的相關方法
+// 伺服器接到任一請求時，server log 上能正確印出時間、印出請求中的 HTTP 與網址
+
+app.get('/', (req, res) => {
+    res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+    res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+    res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+    res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
