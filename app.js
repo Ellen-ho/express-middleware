bb6437cc1ec3ba28cfd1d214e6c51143f2db52f8
Ellen <ellen31725@gmail.com>
2020-11-27 11:22:20 +0800

    check coding style

--- app.js
+++ app.js
@@ -9,30 +9,30 @@ const port = 3000
 // 瞭解時間戳記的相關方法
 // 伺服器接到任一請求時，server log 上能正確印出時間、印出請求中的 HTTP 與網址
 app.use(function (req, res, next) {
-    const timeStamp = dayjs().format('YYYY-MM-DD HH:mm:ss')
-    const requestType = req.method
-    const url = req.originalUrl
-    let log = `${timeStamp} | ${requestType} from ${url}`
-    console.log(log);
-    next();
-});
+  const timeStamp = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  const requestType = req.method
+  const requestUrl = req.originalUrl
+  const log = `${timeStamp} | ${requestType} from ${requestUrl}`
+  console.log(log)
+  next()
+})
 
 app.get('/', (req, res) => {
-    res.send('列出全部 Todo')
+  res.send('列出全部 Todo')
 })
 
 app.get('/new', (req, res) => {
-    res.send('新增 Todo 頁面')
+  res.send('新增 Todo 頁面')
 })
 
 app.get('/:id', (req, res) => {
-    res.send('顯示一筆 Todo')
+  res.send('顯示一筆 Todo')
 })
 
 app.post('/', (req, res) => {
-    res.send('新增一筆  Todo')
+  res.send('新增一筆  Todo')
 })
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}`)
+  console.log(`App running on port ${port}`)
 })
